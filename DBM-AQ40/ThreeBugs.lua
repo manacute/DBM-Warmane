ab40a87692cbe3921943dde3ae77bbfc633d1c04
manacute <maximilian.huang@mail.utoronto.ca>
2023-01-02 20:28:49 -0500

    AQ40: Update timings for three bugs

--- DBM-AQ40/ThreeBugs.lua
+++ DBM-AQ40/ThreeBugs.lua
@@ -22,10 +22,10 @@ local specWarnGTFO		= mod:NewSpecialWarningGTFO(25786, nil, nil, nil, 1, 8)
 
 --"Toxic Volley-25812-npc:15511 = pull:11.8, 13.6, 16.8, 34.1, 14.8, 7.3, 8.3, 12.1, 15.8, 9.7, 19.6, 9.8", -- [12]
 --If users ask for a toxic volley timer, unless classic is different than retail (which i doubt), 7-34 second variable timer is not acceptable
-local timerFearCD		= mod:NewCDTimer(20.5, 26580, nil, nil, nil, 2)
+local timerFearCD		= mod:NewCDTimer(20.6, 26580, nil, nil, nil, 2)
 
 function mod:OnCombatStart(delay)
-	timerFearCD:Start(10-delay)
+	timerFearCD:Start(12-delay)
 	if not self:IsTrivial() then
 		self:RegisterShortTermEvents(
 			"SPELL_AURA_APPLIED 25786 25989",
