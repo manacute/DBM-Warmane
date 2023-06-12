827d60d2badddc2f7384973b2b1dae4fe10806b2
manacute <maximilian.huang@mail.utoronto.ca>
2023-06-11 21:18:41 -0400

    Auchenai Crypts: Add Shirrak timers

--- DBM-Party-BC/Auct_Crypts/Shirrak.lua
+++ DBM-Party-BC/Auct_Crypts/Shirrak.lua
@@ -7,15 +7,35 @@ mod:SetModelID(18916)
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
+	"SPELL_CAST_SUCCESS 32265 32300 36383",
 	"CHAT_MSG_RAID_BOSS_EMOTE"
 )
 
-local warnFocusFire		= mod:NewTargetAnnounce(32300, 3)
+local warnFocusFire			= mod:NewTargetAnnounce(32300, 3)
 
-local specWarnFocusFire	= mod:NewSpecialWarningDodge(32300, nil, nil, nil, 1, 2)
+local timerAttractMagicCD 	= mod:NewCDTimer(30, 32265, nil, nil)
+local timerFocusFireCD 		= mod:NewCDTimer(15, 32300, nil, false)
+local timerBiteCD 			= mod:NewCDTimer(10, 36383, nil, nil)
+
+local specWarnFocusFire		= mod:NewSpecialWarningDodge(32300, nil, nil, nil, 4, 2)
+
+function mod:OnCombatStart(delay)
+	timerAttractMagicCD:Start(28 - delay)
+	timerFocusFireCD:Start(17 - delay)
+	timerBiteCD:Start(10 - delay)
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	if args:IsSpellID(32265) then
+		timerAttractMagicCD:Start()		
+	elseif args:IsSpellID(36383) then
+		timerBiteCD:Start()
+	end
+end
 
 function mod:CHAT_MSG_RAID_BOSS_EMOTE(_, _, _, _, target)
 	local targetname = DBM:GetUnitFullName(target) or target
+	timerFocusFireCD:Start()
 	if targetname == UnitName("player") then
 		specWarnFocusFire:Show()
 		specWarnFocusFire:Play("watchstep")
